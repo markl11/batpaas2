540df32892f75a159146989e0246de5acdc0a042
Mark <mark@mjlnet.com>
2018-06-22 19:50:25 +0100

    updated the script action to remove verbose debugging3

--- script-action.sh
+++ script-action.sh
@@ -1,2 +1,1 @@
-!/bin/bash
 sqoop import --connect 'jdbc:sqlserver://mjlnet210618-1.database.windows.net:1433;database=mjlnet210618-1' --query "select * from SalesLT.Customer where \$CONDITIONS" --username mjlnetadmin  -P --target-dir 'wasb:///data/Adventureworks' --fields-terminated-by '\t' --lines-terminated-by '\n' -m 1
