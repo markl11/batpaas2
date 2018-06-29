f343bdaef2a2f301f617f581ca24a5ba93259935
Mark <mark@mjlnet.com>
2018-06-29 09:38:18 +0100

    Adjusted script action3

--- script-action.sh
+++ script-action.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sqoop import --connect 'jdbc:sqlserver://asql-neu-batdemo-01.database.windows.net:1433;database=db-izcb3vdr43356' --query "select * from dbo.Products where \$CONDITIONS" --username AdminUser -P --target-dir 'wasb:///data/BATPaaSDemoDB1' --fields-terminated-by '\t' --lines-terminated-by '\n' -m 1
+sleep 300
+
+sqoop import --connect 'jdbc:sqlserver://asql-neu-batdemo-01.database.windows.net:1433;database=db-izcb3vdr43356' --query "select * from dbo.Products where \$CONDITIONS" --username AdminUser --password XXXXXXXX --split-by ProductId --columns ProductId,SkuNumber,CategoryId,RecommendationId,Title,Price,SalePrice,ProductArtUrl, Description,Created,ProductDetails,Inventory,LeadTime --target-dir 'wasb:///data/BATPaaSDemoDB1' --fields-terminated-by "\t" --lines-terminated-by '\n' --hive-import --create-hive-table --hive-table BATPaaS1
