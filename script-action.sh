8604c163f7c7280ff9d3885a8e56711185d35465
Mark <mark@mjlnet.com>
2018-06-29 09:40:24 +0100

    Adjusted script action4

--- script-action.sh
+++ script-action.sh
@@ -2,4 +2,4 @@
 
 sleep 300
 
-sqoop import --connect 'jdbc:sqlserver://asql-neu-batdemo-01.database.windows.net:1433;database=db-izcb3vdr43356' --query "select * from dbo.Products where \$CONDITIONS" --username AdminUser --password XXXXXXXX --split-by ProductId --columns ProductId,SkuNumber,CategoryId,RecommendationId,Title,Price,SalePrice,ProductArtUrl, Description,Created,ProductDetails,Inventory,LeadTime --target-dir 'wasb:///data/BATPaaSDemoDB1' --fields-terminated-by "\t" --lines-terminated-by '\n' --hive-import --create-hive-table --hive-table BATPaaS1
+sqoop import --connect 'jdbc:sqlserver://asql-neu-batdemo-01.database.windows.net:1433;database=db-izcb3vdr43356' --query "select * from dbo.Products where \$CONDITIONS" --username AdminUser --password Rhubarbs00 --split-by ProductId --columns ProductId,SkuNumber,CategoryId,RecommendationId,Title,Price,SalePrice,ProductArtUrl, Description,Created,ProductDetails,Inventory,LeadTime --target-dir 'wasb:///data/BATPaaSDemoDB1' --fields-terminated-by "\t" --lines-terminated-by '\n' --hive-import --create-hive-table --hive-table BATPaaS1
