708dbf41467f2c86580f45f6376d913c00a53259
markl11 <38077229+markl11@users.noreply.github.com>
2021-12-02 16:30:16 +0000

    Update script-action.sh

--- script-action.sh
+++ script-action.sh
@@ -1,2 +1,2 @@
-sqoop import --connect 'jdbc:sqlserver://asql-neu-batdemo-01-innov.database.windows.net:1433;database=db-mfyk6hdda5bxs' --query "select * from dbo.Products where \$CONDITIONS" --username AdminUser --password r0a5tb33f123! --target-dir 'wasb:///data/BATPaaSDemoDB1' --fields-terminated-by '\t' --lines-terminated-by '\n' -m 1
+sqoop import --connect 'jdbc:sqlserver://asql-neu-batdemo-01-innov.database.windows.net:1433;database=db-mfyk6hdda5bxs' --query "select * from dbo.Products where \$CONDITIONS" --username AdminUser --password XXXXXXXXXXX --target-dir 'wasb:///data/BATPaaSDemoDB1' --fields-terminated-by '\t' --lines-terminated-by '\n' -m 1
 
